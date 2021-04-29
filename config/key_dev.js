5cfb5205ff6f9ca366e162c23019b8e1eb4ad8c5
emmax <iam4emmax@gmail.com>
2021-04-29 20:18:40 +0100

    single product

--- config/key_dev.js
+++ config/key_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-    FACEBOOK_APP_ID_PRO:"510630313639595",
-    facebook_Secrete_PRO:"0c4da9576d68218e109032ad13d34e77",
+    FACEBOOK_APP_ID_PRO:"289365682667423",
+    facebook_Secrete_PRO:"ae23c331510d534cffa50efb2f3e0d21",
     callbackURL_dev:"http://localhost:5050/users/facebook/callback"
 }
 
