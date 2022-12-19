a243765b2a516872cf7ead1b00108766dbe85d3b
Emmanuel Ayodeji <68588402+iamemmax@users.noreply.github.com>
2022-12-19 01:52:17 +0100

    Update passport.js
    added passport

--- config/passport.js
+++ config/passport.js
@@ -56,6 +56,16 @@ module.exports = function passportAuth (res){
     })       
     )
     
+    
+    passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+       });
+   });
 
     // facebook authentication
     passport.use(new FacebookStrategy({
